f2e5cd78e40166c4a561232a23f6f79e0adcfc59
venuketha15-crypto <venuketha15@gmail.com>
2026-06-11 07:11:53 +0530

    Day 13 - Added Container With Most Water Python solution

---
+++ Day-13-Container-With-Most-Water/ContainerWithMostWater.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            h = min(height[left], height[right])
+            width = right - left
+            area = h * width
+
+            if area > max_area:
+                max_area = area
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
