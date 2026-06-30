210db19a8779098404fb9fc741a41c414c393b86
venuketha15-crypto <venuketha15@gmail.com>
2026-06-30 18:46:58 +0530

    Added Day 32 Python Solution

---
+++ Day-32-Generate-Parentheses/GenerateParentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        stack = []
+        result = []
+
+        def backtrack(openN, closeN):
+
+            if openN == n and closeN == n:
+                result.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closeN)
+                stack.pop()
+
+            if closeN < openN:
+                stack.append(")")
+                backtrack(openN, closeN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+
+        return result
