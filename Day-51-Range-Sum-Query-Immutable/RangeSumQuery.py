c4c0a0157ed570b832a62c267d86ece5c492f24b
venuketha15-crypto <venuketha15@gmail.com>
2026-07-23 21:36:08 +0530

    Added Day 51 Python Solution

---
+++ Day-51-Range-Sum-Query-Immutable/RangeSumQuery.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = [0]
+
+        for num in nums:
+            self.prefix.append(self.prefix[-1] + num)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix[right + 1] - self.prefix[left]
